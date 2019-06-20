27ce70067b1fe3c100955c24c1fa86d3fa989a0e
Numerica98 <00041517@uca.edu.sv>
2019-06-19 20:21:57 -0600

    Agregados los permisos de uso de bluetooth e inicializacion del Server

--- app/src/main/java/com/mapache/bluetoothchat/MainActivity.kt
+++ app/src/main/java/com/mapache/bluetoothchat/MainActivity.kt
@@ -1,12 +1,44 @@
 package com.mapache.bluetoothchat
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothServerSocket
+import android.bluetooth.BluetoothSocket
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.io.IOException
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private val APP_NAME = "BluetoothChat"
+    private val MY_UUID = UUID.fromString("8ce255c0-200a-11e0-ac64-0800200c9a66")
+
+    private var bluetoothAdapter: BluetoothAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+
+    private inner class Server : Thread() {
+        private val serverSocket: BluetoothServerSocket?
+
+        init {
+            var tmp: BluetoothServerSocket? = null
+            try {
+                //Permiso de Bluetooth
+                tmp = bluetoothAdapter!!.listenUsingInsecureRfcommWithServiceRecord(APP_NAME, MY_UUID)
+            } catch (ex: IOException) {
+                ex.printStackTrace()
+            }
+
+            serverSocket = tmp
+        }
+
+        override fun run(){
+            var socket: BluetoothSocket?
+
+        }
     }
 }
